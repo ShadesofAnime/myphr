6c746ca32cf2a4eb06a3abbf42d1057187506584
Steven Hodges <136654425+ShadesofAnime@users.noreply.github.com>
2023-12-01 02:16:42 -0500

    Reload
    Data Scrub. Preparing for final upload

--- js/api/config.js
+++
@@ -1,2 +0,0 @@
-const MY_API_KEY = 'bc4fef14d2mshae21da6d3b08e37p1ac206jsn6850d0e9b28a';
-export { MY_API_KEY };
--- portal/pages/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","shaddhza_appuser","AppUser2024!","shaddhza_myphrTest");
-
-if(!$con){
-    die('Connection Failed'. mysqli_connect_error());
-}
-
-?>
